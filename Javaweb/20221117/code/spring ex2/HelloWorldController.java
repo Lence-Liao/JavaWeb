b00c9b2aa93dff695e4090eec2c42a5ecfe3345c
廖國俊 <113896319+Lence-Liao@users.noreply.github.com>
2022-11-17 16:35:16 +0800

    Add files via upload

---
+++ Javaweb/20221117/code/spring ex2/HelloWorldController.java
@@ -0,0 +1,33 @@
+package springmvc.controller;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloWorldController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String sayHello(ModelMap model) {
+		model.addAttribute("greeting", "Hello World from Spring 5 MVC");
+		return "welcome";
+	}
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String sayHelloAgain(ModelMap model) {
+		model.addAttribute("greeting", "Hello World Again, from Spring 5 MVC");
+		return "welcome";
+	}
+	@RequestMapping(value = "/message", method = RequestMethod.GET)
+	public String getHelloMessage(ModelMap model) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("/demo/example/Beans.xml");
+	    demo.example.HelloWorld obj = (demo.example.HelloWorld) context.getBean("helloWorld");
+        obj.setMessage("HelloWorld Bean Message");
+		model.addAttribute("greeting", obj.getMessage());
+		return "welcome";
+	}
+} 
