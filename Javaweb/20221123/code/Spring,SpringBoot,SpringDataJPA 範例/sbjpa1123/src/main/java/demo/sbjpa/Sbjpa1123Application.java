5a204b94cd8985e1fcb1a7d336fc706121f0d9a2
廖國俊 <113896319+Lence-Liao@users.noreply.github.com>
2022-11-23 16:42:40 +0800

    Add files via upload

---
+++ Javaweb/20221123/code/Spring,SpringBoot,SpringDataJPA 範例/sbjpa1123/src/main/java/demo/sbjpa/Sbjpa1123Application.java
@@ -0,0 +1,13 @@
+package demo.sbjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbjpa1123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbjpa1123Application.class, args);
+	}
+
+}
---
+++ Javaweb/20221123/code/Spring,SpringBoot,SpringDataJPA 範例/sbjpa1123/src/test/java/demo/sbjpa/Sbjpa1123ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.sbjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbjpa1123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
