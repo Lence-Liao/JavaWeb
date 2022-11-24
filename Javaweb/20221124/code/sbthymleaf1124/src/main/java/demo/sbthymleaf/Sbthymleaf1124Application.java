e6fdf4dc44427881b99ee5acf60dba9564d90768
廖國俊 <113896319+Lence-Liao@users.noreply.github.com>
2022-11-24 16:14:18 +0800

    Add files via upload

---
+++ Javaweb/20221124/code/sbthymleaf1124/src/main/java/demo/sbthymleaf/Sbthymleaf1124Application.java
@@ -0,0 +1,13 @@
+package demo.sbthymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbthymleaf1124Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbthymleaf1124Application.class, args);
+	}
+
+}
---
+++ Javaweb/20221124/code/sbthymleaf1124/src/test/java/demo/sbthymleaf/Sbthymleaf1124ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.sbthymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbthymleaf1124ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
